75272bd95b61ca946e636a7eeb12f73a6f80b997
Energetiq <46401261+NergYR@users.noreply.github.com>
2021-01-09 15:57:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> <?php $usernme ?> - Acceuil</title>
+    <title> <?php $username ?> - Acceuil</title>
     <meta name="TITLE" content="NAME">
     <link rel="stylesheet" href="css/style.css" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
