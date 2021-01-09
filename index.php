6fc389a7b00bb05b859c21738e1565da2cd2f8a8
Energetiq <46401261+NergYR@users.noreply.github.com>
2021-01-09 14:29:28 +0100

    Base

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Name - Acceuil</title>
+    <meta name="TITLE" content="NAME">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+
+</head>
+<body>
+
+
+    
+</body>
+</html>
